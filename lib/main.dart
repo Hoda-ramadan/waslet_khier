9558c8f2cc20bed94945566f498176091bf169cd
Huda Ramadan <011hodaramadan@gmail.com>
2026-02-19 18:12:49 -0800

    .

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Waslet_Khier());
+}
+
+class Waslet_Khier extends StatelessWidget {
+  const Waslet_Khier({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
