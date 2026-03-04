b5b6c3e98041e849865f4559c558e7c0aa6bbe78
Parawita <kamalahmedkamal68@gmail.com>
2026-03-04 20:45:52 +0200

    buttom navigation bar done

--- lib/features/main_feature/views/main_view.dart
+++ lib/features/main_feature/views/main_view.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:waslet_khier/const.dart';
 
 class MainScreen extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
-  const MainScreen({super.key, required this.navigationShell});
+  const MainScreen({
+    super.key,
+    required this.navigationShell,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationShell,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: navigationShell.currentIndex,
-        onTap: (index) {
-          navigationShell.goBranch(
-            index,
-            initialLocation: index == navigationShell.currentIndex,
-          );
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "الرئيسية",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.apartment),
-            label: "جمعيات",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "الحالات",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "حسابي",
-          ),
-        ],
+
+      bottomNavigationBar: Directionality(
+        textDirection: TextDirection.rtl, 
+        child: BottomNavigationBar(
+          currentIndex: navigationShell.currentIndex,
+
+          
+          selectedItemColor: appcolor,
+          unselectedItemColor: Colors.grey,
+
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
+
+          onTap: (index) {
+            navigationShell.goBranch(
+              index,
+              initialLocation: index == navigationShell.currentIndex,
+            );
+          },
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "الرئيسية",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.apartment),
+              label: "جمعيات",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "الحالات",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "حسابي",
+            ),
+          ],
+        ),
       ),
     );
   }
