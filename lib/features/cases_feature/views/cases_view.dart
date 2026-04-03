34565f21365487a460c39d88c3719a88400256ab
Parawita <kamalahmedkamal68@gmail.com>
2026-04-03 18:09:44 +0200

    edit states veiw

--- lib/features/cases_feature/views/cases_view.dart
+++ lib/features/cases_feature/views/cases_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:waslet_khier/features/cases_feature/widgets/cases_view_body.dart';
+import 'package:waslet_khier/features/charity_feature/views/widget/customAppbar.dart';
+import 'package:waslet_khier/features/charity_feature/views/widget/custom_app_Bar.dart';
 
 class CasesView extends StatelessWidget {
   const CasesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CasesViewBody();
+    return Column(
+      children: [
+        CustomAppBar(),
+        Expanded(child: CasesViewBody()),
+      ],
+    );
   }
 }
