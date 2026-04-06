e7e7aaafe67a2166ea35e9230ee5898ef8352f11
Huda Ramadan <011hodaramadan@gmail.com>
2026-04-05 22:28:26 -0700

    add 3 splash pages

--- lib/features/splash_feature/widgets/splash_view2.dart
+++ lib/features/splash_feature/widgets/splash_view2.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:waslet_khier/const.dart';
+import 'package:waslet_khier/featureAuth/auth/presintation/view_model/widget/custombuttom.dart';
 import 'package:waslet_khier/features/charity_feature/views/widget/custom_app_Bar.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/customindecator.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/splashview3.dart';
 
 class SplashView2 extends StatelessWidget {
   const SplashView2({super.key});
@@ -25,23 +28,48 @@ class Splash_View_body extends StatelessWidget {
       child: Directionality(
         textDirection: TextDirection.rtl,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "تخطي",
-              style: TextStyle(color: tintAppColor),
-              textAlign: TextAlign.end,
+            GestureDetector(
+              child: Text(
+                "تخطي",
+                style: TextStyle(
+                  color: tintAppColor,
+                  decoration: TextDecoration.underline,
+                  decorationColor: tintAppColor,
+                ),
+                textAlign: TextAlign.start,
+              ),
             ),
-            Image.asset("assets/images/Frame 12.png"),
+            Center(child: Image.asset("assets/images/Frame 12.png")),
             ListTile(
               title: Text(
                 "تبرع بسهولة في أي وقت",
                 style: TextStyle(fontSize: 24, fontFamily: "Roboto"),
+                textAlign: TextAlign.center,
               ),
               subtitle: Text(
-                "ساهم في تغيير حياة شخص محتاج بخطوات بسيطة وسريعة. يسهّل تطبيقنا عليك الوصول للحالات وإتمام التبرع بشكل آمن وشفاف.",
+                "ساهم في تغيير حياة شخص محتاج بخطوات بسيطة وسريعة يسهّل تطبيقنا عليك الوصول للحالات وإتمام التبرع بشكل آمن وشفاف.",
                 style: TextStyle(fontSize: 14, color: Colors.grey),
                 textAlign: TextAlign.center,
               ),
+            ),
+            SizedBox(height: 35),
+            CustomIndicator(currentIndex: 0),
+            SizedBox(height: 35),
+            Custombuttom(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SplashView3();
+                    },
+                  ),
+                );
+              },
+              text: "التالي",
+              color: tintAppColor,
+              textcolor: Colors.white,
             ),
           ],
         ),
---
+++ lib/features/splash_feature/widgets/splashview3.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:waslet_khier/const.dart';
+import 'package:waslet_khier/featureAuth/auth/presintation/view_model/widget/custombuttom.dart';
+import 'package:waslet_khier/features/charity_feature/views/widget/custom_app_Bar.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/customindecator.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/splash_view2.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/splashview3.dart';
+import 'package:waslet_khier/features/splash_feature/widgets/splashview4.dart';
+
+class SplashView3 extends StatelessWidget {
+  const SplashView3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: Splash_View3_body(),
+      backgroundColor: backGroundColor,
+    );
+  }
+}
+
+class Splash_View3_body extends StatelessWidget {
+  const Splash_View3_body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  child: Text(
+                    "تخطي",
+                    style: TextStyle(
+                      color: tintAppColor,
+                      decoration: TextDecoration.underline,
+                      decorationColor: tintAppColor,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SplashView2();
+                        },
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.arrow_forward_ios, color: tintAppColor),
+                ),
+              ],
+            ),
+            Center(child: Image.asset("assets/images/Frame 13.png")),
+            ListTile(
+              title: Text(
+                "مساهمتك بتغير حياة",
+                style: TextStyle(fontSize: 24, fontFamily: "Roboto"),
+                textAlign: TextAlign.center,
+              ),
+              subtitle: Text(
+                "نربط تبرعك بالحالات الأكثر احتياجًا ونقدّم لك طرق مختلفة للمساهمة علشان تقدر تختار الخير اللي تحب توصله.",
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 20),
+            CustomIndicator(currentIndex: 1),
+            SizedBox(height: 20),
+            Custombuttom(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Splashview4();
+                    },
+                  ),
+                );
+              },
+              text: "التالي",
+              color: tintAppColor,
+              textcolor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
