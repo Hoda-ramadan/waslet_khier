02611dbca531e5292457a3985afd607aa3ea8864
Parawita <kamalahmedkamal68@gmail.com>
2026-04-13 18:05:49 +0200

    api servc

--- lib/core/Api/api_service.dart
+++ lib/core/Api/api_service.dart
@@ -1,1 +1,13 @@
 
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio;
+ final String baseurl='http://abdoerfann-001-site1.ntempurl.com/api' ; 
+  ApiService(this.dio);
+
+ Future<dynamic> get({required String endPoint})async {
+    var response = await dio.get(baseurl+endPoint);
+    return response.data;
+  }
+}
