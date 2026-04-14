d27c3eefdb2f1f33f839db07862d12e72c4e1158
Parawita <kamalahmedkamal68@gmail.com>
2026-04-14 21:49:30 +0200

    ww

--- lib/features/charity_feature/data/repo/charity_repo.dart
+++ lib/features/charity_feature/data/repo/charity_repo.dart
@@ -11,4 +11,10 @@ class CharityRepo {
     List charites = data as List; // API returns a List directly
     return charites.map((e) => CharityModel.fromJson(e)).toList();
   }
+
+  Future<List<CharityModel>> users()async{
+    var data = await apiService.getCharites(endPount: '/Charity');
+    List charites = data as List; // API returns a List directly
+    return charites.map((e) => CharityModel.fromJson(e)).toList();
+  }
 }
