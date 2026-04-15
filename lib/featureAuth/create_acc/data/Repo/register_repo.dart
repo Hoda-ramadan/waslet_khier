7a766f0d4145bdcb10225f146b1b7819a1cfc1b7
Huda Ramadan <011hodaramadan@gmail.com>
2026-04-15 12:41:47 -0700

    add model and repo for register

--- lib/featureAuth/create_acc/data/Repo/register_repo.dart
+++ lib/featureAuth/create_acc/data/Repo/register_repo.dart
@@ -1,1 +1,14 @@
-class RegisterRepo {}
+import 'package:waslet_khier/core/Api/api_service.dart';
+import 'package:waslet_khier/featureAuth/create_acc/data/Model/donor_register.dart';
+
+class RegisterRepo {
+  final ApiService apiService;
+
+  RegisterRepo(this.apiService);
+
+  Future<DonorRegister> getregister() async {
+    var data = await apiService.getRegister(endPoint: '/User/Donor-Register');
+
+    return DonorRegister.fromJson(data);
+  }
+}
