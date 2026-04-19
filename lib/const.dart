dd4011e27d9055a2389294a949da467fd2f3523e
Parawita <kamalahmedkamal68@gmail.com>
2026-04-19 03:19:19 +0200

    change colors

--- lib/const.dart
+++ lib/const.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const backGroundColor = Color(0xfff7f7f7);
 const appcolor = Color(0xFF00331f); //تيت من عبدالله 0d001a
 
-const tintAppColor = Color(0xff420080);  //9900ff    //009E60
+const tintAppColor = Color.fromARGB(149, 44, 101, 93);  //9900ff    //009E60
 const orangcolor = Color(0xFF009E60);//ff6600 //003399  //FF8A3D  ثبت من عبالله
 const double kNisab = 85 * 85.0;
